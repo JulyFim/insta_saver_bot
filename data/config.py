7e25186d58a5f586c8f5fa802a16189cab127250
JulyFim <114176355+JulyFim@users.noreply.github.com>
2023-04-08 13:42:20 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -9,8 +9,9 @@ env.read_env()
 BOT_TOKEN = env.str("BOT_TOKEN")  # Bot Token
 ADMINS = env.list("ADMINS")  # adminlar ro'yxati
 
-PROXY_URL = os.environ.get("PROXY_URL")
-DB_USER = env.str("DB_USER")
-DB_PASS = env.str("DB_PASS")
-DB_NAME = env.str("DB_NAME")
-DB_HOST = env.str("DB_HOST")
+DATABASE_URL = env.str("DATABASE_URL")
+PGDATABASE = env.str("PGDATABASE")
+PGHOST = env.str("PGHOST")
+PGPASSWORD = env.str("PGPASSWORD")
+PGPORT = env.str("PGPORT")
+PGUSER = nev.str("PGUSER")
