b403722016853c621c7d82922c68928dae71bdc7
JulyFim <114176355+JulyFim@users.noreply.github.com>
2023-04-09 01:23:22 +0500

    Update app.py

--- app.py
+++ app.py
@@ -7,17 +7,19 @@ from utils.set_bot_commands import set_default_commands
 
 
 async def on_startup(dispatcher):
-    # Ma'lumotlar bazasini yaratamiz:
-    await db.create()
-    # await db.drop_users()
-    await db.create_table_users()
-
     # Birlamchi komandalar (/start va /help)
     await set_default_commands(dispatcher)
 
     # Bot ishga tushgani haqida adminga xabar berish
     await on_startup_notify(dispatcher)
-
+    
+    try:
+        # Ma'lumotlar bazasini yaratamiz:
+        await db.create()
+        # await db.drop_users()
+        await db.create_table_users()
+    except Exception as ex:
+        print(ex)
 
 if __name__ == '__main__':
     executor.start_polling(dp, on_startup=on_startup)
